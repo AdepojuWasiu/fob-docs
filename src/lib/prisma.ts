ea1ffa3938929302fdbde558cbed6d980c4762aa
AdepojuWasiu <adepojuwasiu031@gmail.com>
2026-09-22 03:16:51 +0100

    refactor: prisma client connection with adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,24 +1,46 @@
+// import { PrismaClient } from "@/generated/prisma/client";
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log: ["error"],
+//   });
+
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma;
+// }
+
+// export async function checkDatabaseConnection() {
+//   try {
+//     await prisma.$connect();
+//     console.log("✅ Database connected successfully");
+//   } catch (error) {
+//     console.error("❌ Database connection failed:", error);
+//   }
+// }
+
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log: ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
-}
-
-export async function checkDatabaseConnection() {
-  try {
-    await prisma.$connect();
-    console.log("✅ Database connected successfully");
-  } catch (error) {
-    console.error("❌ Database connection failed:", error);
-  }
 }
